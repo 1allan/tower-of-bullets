13a3b12cbc54e186a032a3c009f37e715cc75f61
Anthony Kamers <anthonybernardo10@gmail.com>
2020-11-19 11:56:42 -0300

    tiro funcionando

--- TowerofBullets/items/bullet.py
+++ TowerofBullets/items/bullet.py
@@ -7,34 +7,23 @@ from utils import *
 class Bullet(Entity):
 
     def __init__(self, surface: pygame.Surface, position: tuple, size: tuple,
-                 image_file: str, damage: int, speed: int = 0):
+                 image_file: str, damage: int, speed: int, coords: list):
         super().__init__(surface, position, size, speed=speed, 
                          image_file=image_file)
         self.damage = damage
+        self.coords = coords
+        self.count = 0
 
     def hit(self):
         pass
 
-    def shoot(self):
-        x_final, y_final = pygame.mouse.get_pos()
-
-        # linear coefficient
-        m = (y_final - self.rect.top) / (x_final - self.rect.left)
-
-        var = -1 if self.rect.top > y_final else 1
-
-        for i in range(self.rect.top, y_final + (1000 * var), var * self.speed):
-            y_next = i
-            x_next = ((y_next - self.rect.top) / m) + self.rect.left
-
-            self.move((x_next, y_next))
-
     def move(self, direction=None):
-        # keeps the same speed in diagonals
-        # self.speed *= math.ceil((direction[0]**2 + direction[1]**2)**.5 / 2)
         self.rect.left = direction[0]
         self.rect.top = direction[1]
-        self.draw()
 
-        # self.rect.top += var * self.speed
-        # self.rect.left = ((self.rect.top + 1 - self.rect.top) / m) + self.rect.left
+    def update(self):
+        try:
+            self.move(self.coords[self.count])
+            self.count += 1
+        except IndexError:
+            pass
--- TowerofBullets/items/weapon.py
+++ TowerofBullets/items/weapon.py
@@ -12,7 +12,24 @@ class Weapon(Entity):
         self.bullets = []
 
     def shoot(self):
-        self.bullets.append(Bullet(self.surface, (self.rect.left, self.rect.top), (6, 6), self.image_bullet, self.damage, 2))
+        coordsBullet = []
+
+        # ver trajetória
+        x_final, y_final = pygame.mouse.get_pos()
+
+        # linear coefficient
+        m = (y_final - self.rect.top) / (x_final - self.rect.left)
+
+        var = -1 if self.rect.top > y_final else 1
+
+        for i in range(self.rect.top, y_final + (1000 * var), var * self.speed):
+            y_next = i
+            x_next = ((y_next - self.rect.top) / m) + self.rect.left
+
+            coordsBullet.append((x_next, y_next))
+
+        bullet = Bullet(self.surface, (self.rect.left, self.rect.top), (6, 6), self.image_bullet, self.damage, 2, coordsBullet)
+        self.bullets.append(bullet)
 
     def change_image_bullet(self, image_bullet: str):
         self.image_bullet = image_bullet
@@ -21,5 +38,8 @@ class Weapon(Entity):
         self.damage = damage
  
     def update(self):
-        for b in self.bullets:
-            b.shoot()
+        for k, bullet in enumerate(self.bullets):
+            if bullet.rect.left == 0 or bullet.rect.top == 0 or bullet.rect.left == 800 or bullet.rect.top == 600:
+                self.bullets.pop(k)
+            else:
+                bullet.draw()
